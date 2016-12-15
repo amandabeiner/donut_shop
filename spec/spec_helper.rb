27df131fb5a1b200d9bd8abbd05feb5475d10bd0
Amanda Beiner <amandabeiner@Amandas-MacBook-Air.local>
2016-12-15 17:34:31 -0500

    spec helper update

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require "date"
+require "pry"
+require "rspec"
+
+require_relative "../lib/fillable"
+require_relative "../lib/donut"
+require_relative "../lib/cookie"
+require_relative "../lib/box"
+require_relative "../lib/eclair"
